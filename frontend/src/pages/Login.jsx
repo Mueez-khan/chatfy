ec1f5b0e6f42622e0ce81705da3e51505157ceb7
Mueez khan <91213819+Mueez-khan@users.noreply.github.com>
2024-12-06 12:39:12 +0500

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -28,7 +28,7 @@ export default function Login() {
       e.preventDefault();
 
       const response = await axios.post(
-        `${import.meta.env.URL}/login`,
+        `${import.meta.env.VITE_URL}/login`,
         userData
       );
 
