ac0b427af28bf08198918456c834b82bf2f529dd
Mueez khan <91213819+Mueez-khan@users.noreply.github.com>
2024-12-07 22:25:30 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 app.use(
     cors({
-      origin: ["http://localhost:5173" , "http://192.168.19.63:5173" , "*"],
+      origin: ["http://localhost:5173" , "http://192.168.19.63:5173" , 'https://chatfy-rven.vercel.app'],
       methods: "GET, POST, PATCH, DELETE, PUT",
       credentials: true,
     })
