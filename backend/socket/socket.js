0cf75a4517ea54e851249f1f1504e73b8b9d0949
Mueez khan <91213819+Mueez-khan@users.noreply.github.com>
2024-12-07 22:25:56 +0500

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 cors: {
-    origin: ["http://localhost:5173" , "http://192.168.19.63:5173" , "*"],
+    origin: ["http://localhost:5173" , "http://192.168.19.63:5173" , 'https://chatfy-rven.vercel.app'],
     methods: ["GET", "POST"],
   },
 });
