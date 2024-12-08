93f8caa5b48c3271b6d4ab4720a61e8ec4c0d493
Mueez khan <91213819+Mueez-khan@users.noreply.github.com>
2024-12-08 18:49:07 +0500

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const { authenticateUser } = require("../middlewares/userMiddleware");
 
 const {register , login } = require("../controllers/userController");
-const {getAllUser } = require("../controllers/getAlluser");
+const {getAllUser , findUserById } = require("../controllers/getAlluser");
 
 router.post("/register" , register);
 router.post("/login" , login);
 router.get("/users"   , authenticateUser , getAllUser);
-
+router.get("/user/data/:userId"   , authenticateUser , findUserById);
 
 
 module.exports = router;
