d89a7ce44c5096d604f7d5973c2c14bd1241223c
Mueez khan <91213819+Mueez-khan@users.noreply.github.com>
2024-12-08 18:43:12 +0500

    Update getAlluser.js

--- backend/controllers/getAlluser.js
+++ backend/controllers/getAlluser.js
@@ -1,5 +1,10 @@
 const User = require("../models/userSchema");
 
+// **********************************************************************
+// **********************************************************************
+//                          Get all user expect the password                               *
+// **********************************************************************
+// **********************************************************************
 
 exports.getAllUser = async (req , res ) =>{
 
@@ -22,6 +27,41 @@ exports.getAllUser = async (req , res ) =>{
         return res.status(500).json({
             success : false,
             message : "Did not get users "
+        })
+    }
+
+}
+
+// **********************************************************************
+// **********************************************************************
+//                          Search user by ID                            *
+// **********************************************************************
+// **********************************************************************
+
+exports.findUserById = async (req , res ) =>{
+
+    try{
+
+        const{ userId }= req.params;
+        // select -password means this will exclude the password field 
+
+        console.log(userId)
+        const usersData = await User.findById(userId).select('-password');;
+
+
+
+        return res.status(200).json({
+            success : true,
+            message : "Got user data",
+            data :  usersData
+        })
+
+    }
+    catch(err){
+        console.log("Error while getting user" , err);
+        return res.status(500).json({
+            success : false,
+            message : "Did not get user "
         })
     }
 
