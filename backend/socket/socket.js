6d3e75b3911aceee5a1dd2868b1748e605cc876c
Mueez khan <91213819+Mueez-khan@users.noreply.github.com>
2024-12-08 22:33:48 +0500

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 cors: {
-    origin: ['https://chatfy-topaz.vercel.app'],
+    origin: 'https://chatfy-topaz.vercel.app',
     withCredentials: true,
   },
 });
