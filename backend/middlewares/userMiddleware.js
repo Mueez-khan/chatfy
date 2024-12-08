12ce51b22a6e906f7c476b45f3257a1b9edd4964
Mueez khan <91213819+Mueez-khan@users.noreply.github.com>
2024-12-08 18:44:23 +0500

    Update userMiddleware.js

--- backend/middlewares/userMiddleware.js
+++ backend/middlewares/userMiddleware.js
@@ -4,17 +4,12 @@ require("dotenv").config();
 
 exports.authenticateUser = (req, res, next) => {
 
-  // const token = req.header("Authorization").replace("Berar" , "") ||
-  //  req.headers.authorization?.split(' ')[1] || req.body.token ||
-    // req.cookies.token ; // this is not working in postman
-  // const token = req.body.token || req.cookies.token || req.header("Authorization").replace("Berar" , "")  || req.headers.authorization?.split(' ')[1]; // this works but this not work with frontend
+  
   const token = 
   req.headers.authorization?.startsWith("Bearer ") ? req.headers.authorization.split(" ")[1] : // From Authorization header
   req.body.token || // From body (if applicable)
   req.cookies.token;
-  // const authHeader = req.headers.authorization;
-  // const token = authHeader && authHeader.split(' ')[1];
-    // console.log("Token in middle" , token);
+
   if (!token) {
     return [res.status(401).json({ message: 'Unauthorized, no token provided' }) , res.redirect('/login')];
   }
@@ -24,7 +19,7 @@ exports.authenticateUser = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWTSECRET); // Verify the token using your JWT secret
-    // console.log("Decode" , decoded);
+  
     req.user = decoded; // Attach the decoded user to the request
     next();
   } catch (error) {
